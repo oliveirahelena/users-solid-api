feceb497c9a6330c4c35576b88d317811a4db292
Helena Oliveira <helena@informach.com.br>
2022-07-13 09:57:33 -0300

    add model and repository

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,13 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const categoryAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists");
+    }
+
+    return this.usersRepository.create({ name, email });
   }
 }
 
